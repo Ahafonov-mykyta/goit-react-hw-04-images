cea126aabaa3d525e3ee62dbfadcb6bf1f02d443
Ahafonov-mykyta <ahafonovmykyta@gmail.com>
2022-08-08 20:20:12 +0300

    +

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -4,10 +4,10 @@ import React from "react";
 export default class Button extends React.Component{
 
     render(){
-       {
-        if(this.props.photo !== null && this.props.photo.length !== 0 && this.props.page !== 50 && this.props.page <= this.props.totalHits/10 ){
-        return  ( <button onClick={this.props.incrementPage} className="button" type="button">Load more</button>)}
-       }
+       
+        if (this.props.photo !== null && this.props.photo.length !== 0 && this.props.page !== 50 && this.props.page <= this.props.totalHits/10 ){
+        return  ( <button onClick={ this.props.incrementPage} className="button" type="button">Load more</button>)}
+       
        }
         
        
